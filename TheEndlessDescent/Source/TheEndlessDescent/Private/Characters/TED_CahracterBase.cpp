a13a1f4ed243a3648d7c0b82852e0e317d1e108b
NicolasDan <n.daniele.3082@gmail.com>
2025-04-29 18:01:29 -0300

    Created CharacterBase, Character_Player and Character_Enemy classes

--- TheEndlessDescent/Source/TheEndlessDescent/Private/Characters/TED_CahracterBase.cpp
+++ TheEndlessDescent/Source/TheEndlessDescent/Private/Characters/TED_CahracterBase.cpp
@@ -1,34 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Characters/TED_CahracterBase.h"
 
-// Sets default values
 ATED_CahracterBase::ATED_CahracterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon Mesh");
+	WeaponMesh->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
-// Called when the game starts or when spawned
 void ATED_CahracterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
-
-// Called every frame
-void ATED_CahracterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ATED_CahracterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
