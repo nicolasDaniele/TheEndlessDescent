f45b1ff84d249223924d7bb1cd75071678dfb478
NicolasDan <n.daniele.3082@gmail.com>
2025-04-30 10:34:21 -0300

    Deleted Binaries, Intermediate and Saved folders

--- TheEndlessDescent/Intermediate/Build/Win64/x64/TheEndlessDescentEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Projects/Unrean/TheEndlessDescent/TheEndlessDescent/Intermediate/Build/Win64/x64/TheEndlessDescentEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- TheEndlessDescent/Intermediate/Build/Win64/x64/TheEndlessDescentEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Projects/Unrean/TheEndlessDescent/TheEndlessDescent/Intermediate/Build/Win64/x64/TheEndlessDescentEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
