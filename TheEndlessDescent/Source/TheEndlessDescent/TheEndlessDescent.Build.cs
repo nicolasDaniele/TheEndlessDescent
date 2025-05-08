69b8a5a03c14089b1ffc4c996ab22a05fd0cfbd8
NicolasDan <n.daniele.3082@gmail.com>
2025-05-08 10:33:37 -0300

    Enabled the GameplayAbilities plugin and modules

--- TheEndlessDescent/Source/TheEndlessDescent/TheEndlessDescent.Build.cs
+++ TheEndlessDescent/Source/TheEndlessDescent/TheEndlessDescent.Build.cs
@@ -8,6 +8,8 @@ public class TheEndlessDescent : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput", "GameplayAbilities" });
+        
+        PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTags" });
     }
 }
