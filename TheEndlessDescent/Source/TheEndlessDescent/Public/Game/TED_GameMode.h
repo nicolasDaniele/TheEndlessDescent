cbb7a7b6bda5d41822a720d22a7c562749399d8c
NicolasDan <n.daniele.3082@gmail.com>
2025-06-25 14:48:32 -0300

    Added a reference for the enemies classes

--- TheEndlessDescent/Source/TheEndlessDescent/Public/Game/TED_GameMode.h
+++ TheEndlessDescent/Source/TheEndlessDescent/Public/Game/TED_GameMode.h
@@ -4,9 +4,14 @@
 #include "GameFramework/GameModeBase.h"
 #include "TED_GameMode.generated.h"
 
+class UDA_TED_CharacterClassInfo;
+
 UCLASS()
 class THEENDLESSDESCENT_API ATED_GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults")
+	TObjectPtr<UDA_TED_CharacterClassInfo> EnemyClassInfo;
 };
