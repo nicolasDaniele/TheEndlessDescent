c9a62aaed454dfd729959f6ae73777822289e616
NicolasDan <n.daniele.3082@gmail.com>
2025-06-25 14:15:34 -0300

    Created an EffectProperties struct and added base attributes

--- TheEndlessDescent/Source/TheEndlessDescent/Private/AbilitySystem/TED_AttributeSet_Base.cpp
+++ TheEndlessDescent/Source/TheEndlessDescent/Private/AbilitySystem/TED_AttributeSet_Base.cpp
@@ -1,11 +1,10 @@
 #include "AbilitySystem/TED_AttributeSet_Base.h"
-#include "AbilitySystemComponent.h"
 #include "Net/UnrealNetwork.h"
-
-UTED_AttributeSet_Base::UTED_AttributeSet_Base()
-{
-
-}
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "TED_GameplayTags.h"
+#include "CombatInterface.h"
 
 void UTED_AttributeSet_Base::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
@@ -13,6 +12,93 @@ void UTED_AttributeSet_Base::GetLifetimeReplicatedProps(TArray<FLifetimeProperty
 
 	DOREPLIFETIME_CONDITION_NOTIFY(UTED_AttributeSet_Base, Health, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UTED_AttributeSet_Base, MaxHealth, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UTED_AttributeSet_Base, Speed, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTED_AttributeSet_Base, Attack, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTED_AttributeSet_Base, Defense, COND_None, REPNOTIFY_Always);
+}
+
+void UTED_AttributeSet_Base::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+}
+
+void UTED_AttributeSet_Base::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FEffectProperties Props;
+	SetEffectProperties(Data, Props);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+	}
+
+	if (Data.EvaluatedData.Attribute == GetIncomingDamageAttribute())
+	{
+		const float LocalIncomingDamage = GetIncomingDamage();
+		SetIncomingDamage(0.0f);
+		if (LocalIncomingDamage > 0.0f)
+		{
+			const float NewHalth = GetHealth() - LocalIncomingDamage;
+			SetHealth(FMath::Clamp(NewHalth, 0.0f, GetMaxHealth()));
+
+			const bool bIsFatal = NewHalth <= 0.0f;
+			if (bIsFatal)
+			{
+				if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetAvatarActor))
+				{
+					CombatInterface->Die();
+				}
+				
+			}
+			else
+			{
+				FGameplayTagContainer TagContainer;
+				TagContainer.AddTag(FTED_GameplayTags::Get().Effect_HitReact);
+				Props.TargetAbilitySystemComponent->TryActivateAbilitiesByTag(TagContainer);
+			}
+		}
+	}
+}
+
+void UTED_AttributeSet_Base::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
+{
+	Props.EffectContextHandle = Data.EffectSpec.GetContext();
+	Props.SourceAbilitySystemComponent = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	if (IsValid(Props.SourceAbilitySystemComponent) && Props.SourceAbilitySystemComponent->AbilityActorInfo.IsValid() &&
+		Props.SourceAbilitySystemComponent->AbilityActorInfo->AvatarActor.IsValid())
+	{
+		Props.SourceAvatarActor = Props.SourceAbilitySystemComponent->AbilityActorInfo->AvatarActor.Get();
+		Props.SourceController = Props.SourceAbilitySystemComponent->AbilityActorInfo->PlayerController.Get();
+
+		if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
+		{
+			if (const APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
+			{
+				Props.SourceController = Pawn->GetController();
+			}
+		}
+		if (Props.SourceController)
+		{
+			Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
+		}
+	}
+
+	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
+		Props.TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
+	}
 }
 
 void UTED_AttributeSet_Base::OnRep_Health(const FGameplayAttributeData& OldHealth) const
@@ -23,4 +109,19 @@ void UTED_AttributeSet_Base::OnRep_Health(const FGameplayAttributeData& OldHealt
 void UTED_AttributeSet_Base::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UTED_AttributeSet_Base, MaxHealth, OldMaxHealth);
+}
+
+void UTED_AttributeSet_Base::OnRep_Speed(const FGameplayAttributeData& OldSpeed) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTED_AttributeSet_Base, Speed, OldSpeed);
+}
+
+void UTED_AttributeSet_Base::OnRep_Attack(const FGameplayAttributeData& OldAttack) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTED_AttributeSet_Base, Attack, OldAttack);
+}
+
+void UTED_AttributeSet_Base::OnRep_Defense(const FGameplayAttributeData& OldDefense) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTED_AttributeSet_Base, Defense, OldDefense);
 }
--- TheEndlessDescent/Source/TheEndlessDescent/Public/AbilitySystem/TED_AttributeSet_Base.h
+++ TheEndlessDescent/Source/TheEndlessDescent/Public/AbilitySystem/TED_AttributeSet_Base.h
@@ -2,7 +2,42 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "TED_AttributeSet_Base.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+USTRUCT()
+struct FEffectProperties
+{
+	GENERATED_BODY()
+
+	FEffectProperties() { }
+
+	FGameplayEffectContextHandle EffectContextHandle;
+
+	UPROPERTY() 
+	UAbilitySystemComponent* SourceAbilitySystemComponent = nullptr;
+	UPROPERTY() 
+	AActor* SourceAvatarActor = nullptr;
+	UPROPERTY() 
+	AController* SourceController = nullptr;
+	UPROPERTY() 
+	ACharacter* SourceCharacter = nullptr;
+
+	UPROPERTY() 
+	UAbilitySystemComponent* TargetAbilitySystemComponent = nullptr;
+	UPROPERTY() 
+	AActor* TargetAvatarActor = nullptr;
+	UPROPERTY() 
+	AController* TargetController = nullptr;
+	UPROPERTY() 
+	ACharacter* TargetCharacter = nullptr;
+};
 
 UCLASS()
 class THEENDLESSDESCENT_API UTED_AttributeSet_Base : public UAttributeSet
@@ -10,16 +45,47 @@ class THEENDLESSDESCENT_API UTED_AttributeSet_Base : public UAttributeSet
 	GENERATED_BODY()
 	
 public:
-	UTED_AttributeSet_Base();
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	UTED_AttributeSet_Base() { }
 
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health)
-	FGameplayAttributeData Health;
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth)
-	FGameplayAttributeData MaxHealth;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 	UFUNCTION()
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 	UFUNCTION()
 	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Speed(const FGameplayAttributeData& OldSpeed) const;
+	UFUNCTION()
+	void OnRep_Attack(const FGameplayAttributeData& OldAttack) const;
+	UFUNCTION()
+	void OnRep_Defense(const FGameplayAttributeData& OldAttack) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UTED_AttributeSet_Base, Health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UTED_AttributeSet_Base, MaxHealth);
+
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Speed)
+	FGameplayAttributeData Speed;
+	ATTRIBUTE_ACCESSORS(UTED_AttributeSet_Base, Speed);
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Attack)
+	FGameplayAttributeData Attack;
+	ATTRIBUTE_ACCESSORS(UTED_AttributeSet_Base, Attack);
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Defense)
+	FGameplayAttributeData Defense;
+	ATTRIBUTE_ACCESSORS(UTED_AttributeSet_Base, Defense);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Meta Attributes")
+	FGameplayAttributeData IncomingDamage;
+	ATTRIBUTE_ACCESSORS(UTED_AttributeSet_Base, IncomingDamage);
+
+private:
+	void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 };
