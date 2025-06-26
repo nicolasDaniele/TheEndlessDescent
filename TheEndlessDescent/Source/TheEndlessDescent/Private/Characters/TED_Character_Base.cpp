1e2800a4df5a11d661b10efc7b583d1b4bd42e7c
NicolasDan <n.daniele.3082@gmail.com>
2025-06-26 20:34:36 -0300

    Initialize collisions and handle death

--- TheEndlessDescent/Source/TheEndlessDescent/Private/Characters/TED_Character_Base.cpp
+++ TheEndlessDescent/Source/TheEndlessDescent/Private/Characters/TED_Character_Base.cpp
@@ -1,8 +1,18 @@
 #include "Characters/TED_Character_Base.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/TED_AbilitySystemComponent_Base.h"
+#include "Components/CapsuleComponent.h"
+#include "TED.h"
 
 ATED_Character_Base::ATED_Character_Base()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetCapsuleComponent()->SetGenerateOverlapEvents(false);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile, ECR_Overlap);
+	GetMesh()->SetGenerateOverlapEvents(true);
 }
 
 UAbilitySystemComponent* ATED_Character_Base::GetAbilitySystemComponent() const
@@ -13,4 +23,54 @@ UAbilitySystemComponent* ATED_Character_Base::GetAbilitySystemComponent() const
 UAttributeSet* ATED_Character_Base::GetAttributeSet() const
 {
 	return AttributeSet;
+}
+
+void ATED_Character_Base::InitializeAttributes()
+{
+	check(IsValid(GetAbilitySystemComponent()));	
+	if (DefaultAttributes == nullptr)
+	{
+		return;
+	}
+
+	FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	ContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(DefaultAttributes, 1.0f, ContextHandle);
+	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
+}
+
+void ATED_Character_Base::AddCharacterStartupAbilities()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	UTED_AbilitySystemComponent_Base* TedAbilitySystemComponent = CastChecked<UTED_AbilitySystemComponent_Base>(AbilitySystemComponent);
+	TedAbilitySystemComponent->AddCharacterAbilities(StartupAbilities);
+}
+
+FVector ATED_Character_Base::GetCombatSocketLocation()
+{
+	return GetMesh()->GetSocketLocation(CombatSocketName);
+}
+
+UAnimMontage* ATED_Character_Base::GetHitReactMontage_Implementation()
+{
+	return HitReactMontage;
+}
+
+void ATED_Character_Base::Die()
+{
+	MulticastHandleDeath();
+}
+
+void ATED_Character_Base::MulticastHandleDeath_Implementation()
+{
+	GetMesh()->SetSimulatePhysics(true);
+	GetMesh()->SetEnableGravity(true);
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
--- TheEndlessDescent/Source/TheEndlessDescent/Public/Characters/TED_Character_Base.h
+++ TheEndlessDescent/Source/TheEndlessDescent/Public/Characters/TED_Character_Base.h
@@ -3,13 +3,17 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
+#include "CombatInterface.h"
 #include "TED_Character_Base.generated.h"
 
 class UAbilitySystemComponent;
 class UAttributeSet;
+class UGameplayEffect;
+class UGameplayAbility;
+class UAnimMontage;
 
 UCLASS()
-class THEENDLESSDESCENT_API ATED_Character_Base : public ACharacter, public IAbilitySystemInterface
+class THEENDLESSDESCENT_API ATED_Character_Base : public ACharacter, public IAbilitySystemInterface, public ICombatInterface
 {
 	GENERATED_BODY()
 
@@ -18,10 +22,32 @@ public:
 
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const;
+	
+	virtual FVector GetCombatSocketLocation() override;
+	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
+	virtual void Die() override;
+
+	UFUNCTION(NetMulticast, Reliable)
+	virtual void MulticastHandleDeath();
 
 protected:
+	virtual void InitAbilityActorInfo() { }
+	virtual void InitializeAttributes();
+	void AddCharacterStartupAbilities();
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	FName CombatSocketName;
+
 	UPROPERTY()
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 	UPROPERTY()
 	TObjectPtr <UAttributeSet> AttributeSet;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attributes")
+	TSubclassOf<UGameplayEffect> DefaultAttributes;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Abilities")
+	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<UAnimMontage> HitReactMontage;
 };
